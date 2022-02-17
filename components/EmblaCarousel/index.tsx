d7231c33dc03205834f2d8f3d007ee659538fb04
James Morris <james-morris2010@hotmail.co.uk>
2022-02-17 01:58:58 +0000

    Add EmblaCarousel component

---
+++ components/EmblaCarousel/index.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useCallback } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Image from "next/image";
+import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
+
+interface IProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+const EmblaCarousel: React.FC<IProps> = ({ children }): React.ReactElement => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
+
+  useEffect(() => {
+    if (emblaApi) {
+      // Embla API is ready
+    }
+  }, [emblaApi]);
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  return (
+    <div className="embla-carousel relative">
+      <div className="embla relative">
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container space-x-5 px-20 py-10">
+            {children}
+          </div>
+        </div>
+      </div>
+      <button
+        className="embla__prev -left-10 rounded-full h-14 w-14 bg-playstation-primary flex justify-center items-center"
+        onClick={scrollPrev}
+      >
+        <IoChevronBackOutline color="#ffffff" className="text-4xl" />
+      </button>
+      <button
+        className="embla__next -right-10 rounded-full h-14 w-14 bg-playstation-primary flex justify-center items-center"
+        onClick={scrollNext}
+      >
+        <IoChevronForwardOutline color="#ffffff" className="text-4xl" />
+      </button>
+    </div>
+  );
+};
+
+export default EmblaCarousel;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,90 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 // Types
-import type { NextPage } from 'next';
+import type { NextPage } from "next";
 // Third party
-import Prismic from 'prismic-javascript';
-import { RichText } from 'prismic-reactjs';
-import { Client } from '../prismic.config';
+import Prismic from "prismic-javascript";
+import { RichText } from "prismic-reactjs";
+import { Client } from "../prismic.config";
 // Styling
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
 // Components
-import Head from 'next/head';
-import Layout from '../components/Layout';
-import Link from 'next/link';
-import Image from 'next/image';
-import SectionWrapper from '../components/SectionWrapper';
-import SeeMore from '../components/SeeMore';
-import WelcomeTo from '../components/WelcomeTo';
-import PromoSection1 from '../components/PromoSection1';
-import PromoHeading1 from '../components/PromoHeading1';
+import Head from "next/head";
+import Layout from "../components/Layout";
+import Link from "next/link";
+import Image from "next/image";
+import SectionWrapper from "../components/SectionWrapper";
+import SeeMore from "../components/SeeMore";
+import WelcomeTo from "../components/WelcomeTo";
+import PromoSection1 from "../components/PromoSection1";
+import PromoHeading1 from "../components/PromoHeading1";
+import EmblaCarousel from "../components/EmblaCarousel";
 
 interface IProps {
   homepage: any;
 }
 
+const newGames = [
+  "https://image.api.playstation.com/vulcan/ap/rnd/202201/2109/jx6KJhJn0NUTVdha0a1PA4KQ.png?w=440&thumb=false",
+  "https://image.api.playstation.com/vulcan/ap/rnd/202201/2109/jx6KJhJn0NUTVdha0a1PA4KQ.png?w=440&thumb=false",
+  "https://image.api.playstation.com/vulcan/ap/rnd/202201/2109/jx6KJhJn0NUTVdha0a1PA4KQ.png?w=440&thumb=false",
+  "https://image.api.playstation.com/vulcan/ap/rnd/202201/2109/jx6KJhJn0NUTVdha0a1PA4KQ.png?w=440&thumb=false",
+  "https://image.api.playstation.com/vulcan/ap/rnd/202201/2109/jx6KJhJn0NUTVdha0a1PA4KQ.png?w=440&thumb=false",
+  "https://image.api.playstation.com/vulcan/ap/rnd/202201/2109/jx6KJhJn0NUTVdha0a1PA4KQ.png?w=440&thumb=false",
+  "https://image.api.playstation.com/vulcan/ap/rnd/202201/2109/jx6KJhJn0NUTVdha0a1PA4KQ.png?w=440&thumb=false",
+  "https://image.api.playstation.com/vulcan/ap/rnd/202201/2109/jx6KJhJn0NUTVdha0a1PA4KQ.png?w=440&thumb=false",
+];
+
 const Home: NextPage<IProps> = ({ homepage }) => {
   useEffect(() => {
-    console.log('Homepage.props.homepage', homepage);
+    console.log("Homepage.props.homepage", homepage);
   }, []);
 
   return (
     <div>
       <Head>
         <title>Latest | Official Playstation Store</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Layout>
         <>
-          <div className='h-20 w-full bg-gray-200' />
+          <div className="h-20 w-full bg-gray-200" />
           <div key={homepage.uid}>
-            <div className='relative w-full border border-red-900 h-112'>
+            <div className="relative w-full border border-red-900 h-112">
               <Image
                 src={homepage.data.promoimage1[0].text}
-                layout='fill'
-                objectFit='cover'
-                alt='header'
+                layout="fill"
+                objectFit="cover"
+                alt="header"
               />
             </div>
             <PromoHeading1 homepage={homepage} />
             <PromoSection1 homepage={homepage} />
-            <div className='relative w-full h-120'>
+            <div className="relative w-full h-120">
               <Image
                 src={homepage.data.promoimage2[0].text}
-                layout='fill'
-                objectFit='cover'
-                alt='promo'
+                layout="fill"
+                objectFit="cover"
+                alt="promo"
               />
             </div>
+            <section className="flex flex-col my-20 px-20">
+              <h2 className="text-3xl font-thin">New Games</h2>
+              <EmblaCarousel>
+                {newGames.map((game, index) => (
+                  <div className="embla__slide" key={`${game}-${index}`}>
+                    <div className="h-64 w-64 relative rounded-2xl overflow-hidden">
+                      <Image
+                        src={game}
+                        layout="fill"
+                        objectFit="contain"
+                        alt="carousel"
+                      />
+                    </div>
+                  </div>
+                ))}
+              </EmblaCarousel>
+            </section>
             <WelcomeTo />
             <SeeMore />
           </div>
@@ -67,7 +96,7 @@ const Home: NextPage<IProps> = ({ homepage }) => {
 
 export async function getServerSideProps() {
   const homepages = await Client().query(
-    Prismic.Predicates.at('document.type', 'homepage')
+    Prismic.Predicates.at("document.type", "homepage")
   );
 
   return {
