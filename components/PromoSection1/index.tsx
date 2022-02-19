8c455493b596342b51c1d49bfce6ca67f3e489dc
James Morris <james-morris2010@hotmail.co.uk>
2022-02-19 21:03:25 +0000

    Update PromoSection1 render logic

--- components/PromoSection1/index.tsx
+++ components/PromoSection1/index.tsx
@@ -1,42 +1,57 @@
+import { useState } from "react";
 // Components
-import Image from 'next/image';
-import React from 'react';
-import SectionWrapper from '../SectionWrapper';
+import Image from "next/image";
+import React, { useEffect } from "react";
+import SectionWrapper from "../SectionWrapper";
 
 interface IProps {
   homepage: any;
 }
 
 const PromoSection1: React.FC<IProps> = ({ homepage }): React.ReactElement => {
+  const [promoTiles, setPromoTiles] = useState<any>([]);
+
+  useEffect(() => {
+    const promoTilesToRender = homepage.data.body.filter(
+      (slice) => slice.slice_type === "promotiles1"
+    );
+
+    // debugger;
+
+    setPromoTiles(promoTilesToRender);
+  }, []);
+
   return (
-    <div className='bg-gray-200'>
+    <div className="bg-gray-200">
       <SectionWrapper>
-        <div className='promo-section-1 flex justify-center py-20'>
-          {Object.entries(homepage.data.promoimages1[0]).map(
-            (promoImage: any, index: number) => {
-              const totalImages = Object.entries(
-                homepage.data.promoimages1[0]
-              ).length;
+        {promoTiles.length && (
+          <div className="promo-section-1 flex justify-center py-20">
+            {promoTiles[0].items.map((promoImage: any, index: number) => {
+              const totalImageLength = promoTiles.length;
+
+              console.log(promoImage);
+
               return (
-                <div
-                  className={`relative w-1/4 border border-red-900 ${
-                    index + 1 !== totalImages ? 'mr-5' : 'mr-0'
-                  } rounded-xl overflow-hidden`}
-                >
-                  <div className='image-overlay z-10 hover:bg-black hover:bg-opacity-10 cursor-pointer absolute top-0 left-0 h-full w-full' />
-                  <Image
-                    src={promoImage[1][0].text}
-                    objectFit='cover'
-                    layout='responsive'
-                    width='100%'
-                    height='100%'
-                    alt='promo'
-                  />
-                </div>
+                <>
+                  <div
+                    className={`relative w-1/4 border border-red-900 mx-3 rounded-xl overflow-hidden`}
+                    key={promoImage.imgUrl}
+                  >
+                    <div className="image-overlay z-10 hover:bg-black hover:bg-opacity-10 cursor-pointer absolute top-0 left-0 h-full w-full" />
+                    <Image
+                      src={promoImage.imgurl[0].text}
+                      objectFit="cover"
+                      layout="responsive"
+                      width="100%"
+                      height="100%"
+                      alt="promo"
+                    />
+                  </div>
+                </>
               );
-            }
-          )}
-        </div>
+            })}
+          </div>
+        )}
       </SectionWrapper>
     </div>
   );
